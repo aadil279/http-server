6f0340d2e4380ae7bdc7af2cf2e493ed7101c3e6
Aadil Sidik <aadilsidik2@gmail.com>
2025-06-18 12:58:12 +0100

    feat: Basic HTTP response on handleConnetion

--- main.go
+++ main.go
@@ -39,22 +39,38 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
+	// Close connection if interrupted
 	defer conn.Close()
 
 	// Allocate 1024 bytes in memory for connection buffer
 	buf := make([]byte, 1024)
 
 	// While the connection is alive, echo back any messages received
-	for {
-		n, err := conn.Read(buf)
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	n, err := conn.Read(buf)
 
-		// Echo back the message we just received
-		message := string(buf[:n])
-		conn.Write([]byte("Message received: " + message))
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	// Print back the message we just received
+	fmt.Println(string(buf[:n]))
+
+	// HTTP repsonse
+	message := `
+	HTTP/1.1 200 OK
+	Date: Tue, 22 Jun 2024 18:30:00 GMT
+	Content-Type: text/html; charset=UTF-8
+	Content-Length: 1234
+
+	<html>
+		<body>
+		<h1>Hello, World!</h1>
+		</body>
+	</html>`
+
+	// Send the response back
+	conn.Write([]byte(message))
+	conn.Close()
 }
